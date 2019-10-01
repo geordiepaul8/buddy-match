92b0d220f596157b2fb62cf93b97e5a0d13d92f7
Paul Costigan <paul.costigan@engineering.digital.dwp.gov.uk>
2019-10-01 13:28:36 +0100

    chore: added docker compose

--- compatibilities/index.js
+++ compatibilities/index.js
@@ -4,33 +4,34 @@
 const express = require('express')
 const app = express()
 const mongoose = require('mongoose')
-
-
-
-
-const port = 3000;
+require('dotenv').config();
 
 const logger = require('./utils/logger');
 
 
 
+const APP_PORT = process.env.APPLICATION_PORT || 3000;
 
-
-const MONGO_PORT = 27017;
-const MONGO_HOST = '127.0.0.1';
-const MONGO_DB = 'MyDb';
+const MONGO_PORT = process.env.MONGO_PORT;
+const MONGO_HOST = 'mongo'; //process.env.MONGO_HOST
+const MONGO_DB = process.env.MONGO_DB;
 
 /*
 **  mongoDb connection details
 */
-
-mongoose.set('useCreateIndex', true)
-
-mongoose.connect(`mongodb://${MONGO_HOST}:${MONGO_PORT}/${MONGO_DB}`, { useNewUrlParser: true });
-mongoose.set('useFindAndModify', false);
-mongoose.set('useNewUrlParser', true);
-
-logger.info(`mongoDb running on ${MONGO_HOST}, port: ${MONGO_PORT}, using db: ${MONGO_DB}`)
+mongoose.connect(`mongodb://${MONGO_HOST}:${MONGO_PORT}/${MONGO_DB}`, { 
+  useUnifiedTopology: true, 
+  useNewUrlParser: true,
+  useFindAndModify: false,
+  useCreateIndex: true
+})
+.then( () => {
+  logger.info(`mongoDb running on ${MONGO_HOST}, port: ${MONGO_PORT}, using db: ${MONGO_DB}`)
+})
+.catch( (err) => {
+  logger.error(`error connecting to db: ${err}`);
+  throw new Error(`error connecting to mongoDb running on ${MONGO_HOST}, port: ${MONGO_PORT}, using db: ${MONGO_DB}`)
+})
 
 
 app.all('/*', function(req, res, next) {
@@ -58,8 +59,10 @@ app.use('/v1/matches', matchesRoutes);
 
  
 app.get('/', function (req, res) {
+  logger.info(`sending hello world`)
   res.send('Hello World')
+  
 })
 
-logger.info(`Server running on port: ${port}`);
-app.listen(port)
+logger.info(`Server running on port: ${APP_PORT}`);
+app.listen(APP_PORT)
